80f4345322ffeb745a974940714e61571793d982
fitowashere <fito_washere@icloud.com>
2024-01-05 20:17:12 -0400

    output

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -8,3 +8,11 @@ def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
         return lst[0]
     else:
         return None
+
+
+# Manually setting the annotations to display Optional as desired
+    # if not Optional will print out typing.Union
+safe_first_element.__annotations__ = {
+    'lst': 'typing.Sequence[typing.Any]',
+    'return': 'typing.Optional[typing.Any]'
+}
