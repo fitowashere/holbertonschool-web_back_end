6a4a50795a4b3f146b476a8c155567061da07e20
fitowashere <fito_washere@icloud.com>
2024-01-05 20:24:43 -0400

    validation

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -4,15 +4,10 @@ from typing import Sequence, Any, Optional
 
 
 def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+    """return values with the appropriate types"""
     if lst:
         return lst[0]
     else:
         return None
 
 
-# Manually setting the annotations to display Optional as desired
-    # if not Optional will print out typing.Union
-safe_first_element.__annotations__ = {
-    'lst': 'typing.Sequence[typing.Any]',
-    'return': 'typing.Optional[typing.Any]'
-}
