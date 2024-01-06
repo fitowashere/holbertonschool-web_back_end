1b45f8e158c972e0377015705e124ce9363c60f4
fitowashere <fito_washere@icloud.com>
2024-01-05 20:31:14 -0400

    correcting output

--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 """return values with the appropriate types"""
-
-
 from typing import Mapping, Any, TypeVar, Union
 
 
 # Define a type variable
-T = TypeVar
+T = TypeVar('T')
 m = Mapping
 u = Union
 a = Any
 
 
 def safely_get_value(dct: m, key: a, default: u[T, None] = None) -> u[a, T]:
+    """return values with the appropriate types"""
+
     if key in dct:
         return dct[key]
     else:
