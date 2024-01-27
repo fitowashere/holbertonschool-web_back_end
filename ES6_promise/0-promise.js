174f90ab7d09c471cb8907a12c2ca88bcad576ad
fitowashere <fito_washere@icloud.com>
2024-01-26 23:29:29 -0400

    test

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,8 @@
+export default function getResponseFromAPI() {
+  // eslint-disable-next-line
+      return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve('true');
+      });
+    });
+  }
